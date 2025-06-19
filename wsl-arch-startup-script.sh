13a2fcc2225556d2884b679dfaae6a8e66c4307c
bernelius <bob@bernelius.com>
2025-06-19 23:53:39 +0200

    double check inputs, whitespace around inputs

--- wsl-arch-startup-script.sh
+++ wsl-arch-startup-script.sh
@@ -6,6 +6,16 @@
 #set -x will unsilence commands
 #set -x
 
+prompt_username() {
+	echo "------------------------"
+	echo ""
+	read -rp "Enter your new username: " USERNAME
+	echo ""
+	echo "------------------------"
+}
+
+
+
 echo "Installing packages..."
 pacman -Syu --noconfirm \
 	sudo \
@@ -41,10 +51,22 @@ fi
 #uncommenting sudo line in sudoers
 sed -i 's/^#\s*\(%sudo\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers
 
-echo "------------------------"
-echo ""
-read -rp "Enter your new username: " USERNAME
-echo ""
+while true; do
+	prompt_username
+	echo "Is this username correct: '$USERNAME'? (Y/n)"
+	read answer
+	answer=${answer:-Y}
+	case "$answer" in
+		[Yy]*)
+			break
+			;;
+		[Nn]*)
+			;;
+		*)
+			echo "Please enter y or n."
+			;;
+	esac
+done
 
 useradd -m -G sudo "$USERNAME"
 
@@ -63,39 +85,78 @@ runuser -l "$USERNAME" -c '
 	makepkg -si --noconfirm
 	rm -rf ~/yay-git
 '
+#if wsl, clone win32yank
+if grep -qi microsoft /proc/sys/kernel/osrelease; then
+	mkdir -p /usr/local/bin
+	cd /tmp
+	curl -LO https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-x64.zip
+	unzip win32yank-x64.zip
+	mv win32yank.exe /usr/local/bin/
+	chmod +x /usr/local/bin/win32yank.exe
+fi
 
 #sets up github ssh and clones the bernelius/dotfiles repo
 runuser -l "$USERNAME" -c '
+	prompt_github_details() {
+		echo "------------------------"
+		echo ""
+		read -rp "Enter github email address: " EMAIL
+		read -rp "Enter github name to use for commits: " GITNAME
+		echo ""
+		echo "------------------------"
+	}
+	
 	cd ~
 	git clone https://github.com/bernelius/dotfiles
 	cd dotfiles
 	chmod +x install.sh
 	./install.sh
 	git remote set-url origin git@github.com:bernelius/dotfiles.git
+	
+	while true; do
+		prompt_github_details
+		echo "Is this information correct:"
+		echo "github email address: $EMAIL"
+		echo "github commit name: $GITNAME"
+		echo "(Y/n)"
+		read answer
+		answer=${answer:-Y}
+		case "$answer" in
+			[Yy]*)
+				break
+				;;
+			[Nn]*)
+				;;
+			*)
+				echo "Please enter y or n."
+				;;
+		esac
+	done
+	
 
-	echo "------------------------"
-	echo ""
-	read -rp "Enter github email address: " EMAIL
-	read -rp "Enter github name to use for commits: " GITNAME
-	echo ""
 
 	git config --global user.email "$EMAIL"
 	git config --global user.name "$GITNAME"
 
-	ssh-keygen -t ed25519 -C "$EMAIL" -f ~/.ssh/id_ed25519
+	ssh-keygen -t ed25519 -C "$EMAIL" -f ~/.ssh/id_ed25519 -N ""
 	eval `ssh-agent`
 	ssh-add ~/.ssh/id_ed25519
 	cat ~/.ssh/id_ed25519.pub | win32yank.exe -i
-	echo "------------------------"
+	echo ""
+	echo ""
 	echo "ssh key copied to windows clipboard. Go to github.com/settings/keys to paste it"
-	echo "------------------------"
+	echo ""
+	echo ""
 '
 
 #deletes temporary passwordless sudo access
 rm /etc/sudoers.d/temp_user_010101
 echo "------------------------"
 echo ""
+echo "Set up a password for your linux user account ($USERNAME)"
 passwd "$USERNAME"
+echo ""
 echo "------------------------"
 su - "$USERNAME"
 chsh -s /bin/zsh
+echo "Welcome."
